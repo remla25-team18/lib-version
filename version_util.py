85bd515236f37525b9046450486c46039c7db31b
Madalena Barroso Gomes Pereira Placido <M.BarrosoGomesPereiraPlacido@student.tudelft.nl>
2025-05-05 11:17:12 +0200

    show current version

--- version_util.py
+++ version_util.py
@@ -6,7 +6,7 @@ class VersionUtil:
     @staticmethod
     def get_version():
         try:
-            with open("version.txt", "r") as f:
+            with open("VERSION", "r") as f:
                 return f.read().strip()
         except Exception:
             return "unknown"
