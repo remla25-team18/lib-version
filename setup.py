0751d8fb6ffed7caa346ac894c7c3f49f253c4d6
Madalena Barroso Gomes Pereira Placido <M.BarrosoGomesPereiraPlacido@student.tudelft.nl>
2025-05-06 22:13:10 +0200

    update packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup, find_packages
-from lib_version_remla25_team18.version import __version__
 
 setup(
-    name='lib_version_remla25_team18',
-    version=__version__,
+    name="lib_version",
+    version="0.0.3",
     packages=find_packages(),
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "lib_version = lib_version:VersionUtil.print_version"
-        ]
-    }
 )
