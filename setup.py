23cfeb64fb4090e4e5aec5cecc571abe53dcdce8
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-05-13 22:36:19 +0200

    testing versions

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
+import os
+import re
+
+def read_version():
+    with open(os.path.join("lib_version", "version.py")) as f:
+        match = re.search(r'^__version__ = ["\']([^"\']*)["\']', f.read(), re.M)
+        if match:
+            return match.group(1)
+        raise RuntimeError("Unable to find version string.")
 
 setup(
-    name="lib_version",
-    version="0.0.3",
+    name="lib-version",
+    version=read_version(),
     packages=find_packages(),
 )
